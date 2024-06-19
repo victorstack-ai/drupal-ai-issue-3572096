c84e87e276f7c2191977a5c50bb06f6f9fdc3b75
Marcus Johansson <me@marcusmailbox.com>
2024-06-19 15:40:42 +0200

    Targets for Drupal CI

--- src/Plugin/ProviderProxy.php
+++ src/Plugin/ProviderProxy.php
@@ -118,7 +118,6 @@ class ProviderProxy {
     $this->plugin->setTag($operation_type);
     $this->plugin->setTag($arguments[1]);
 
-
     // Invoke the pre generate response event.
     $pre_generate_event = new PreGenerateResponseEvent($this->plugin->getPluginId(), $operation_type, $this->plugin->configuration, $arguments[0], $arguments[1], $arguments[2]);
     $this->eventDispatcher->dispatch($pre_generate_event, PreGenerateResponseEvent::EVENT_NAME);
