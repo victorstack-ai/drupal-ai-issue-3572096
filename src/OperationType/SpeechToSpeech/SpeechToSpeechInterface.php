e6b36e4c661ecd8d4ee469f1c13f15cfd13a00f0
Marcus Johansson <me@marcusmailbox.com>
2024-07-10 16:19:08 +0200

    Issue #3460556 by Marcus_Johansson: Add Speech-to-Speech operation type

---
+++ src/OperationType/SpeechToSpeech/SpeechToSpeechInterface.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\ai\OperationType\SpeechToSpeech;
+
+use Drupal\ai\Attribute\OperationType;
+use Drupal\ai\OperationType\OperationTypeInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+
+/**
+ * Interface for speech to speech models.
+ */
+#[OperationType(
+  id: 'speech_to_speech',
+  label: new TranslatableMarkup('Speech to Speech'),
+)]
+interface SpeechToSpeechInterface extends OperationTypeInterface {
+
+  /**
+   * Generate speech to speech.
+   *
+   * @param string|array|\Drupal\ai\Operation\SpeechToSpeech\SpeechToSpeechInput $input
+   *   The audio to generate audio from or a binary.
+   * @param string $model_id
+   *   The model id to use.
+   * @param array $tags
+   *   Extra tags to set.
+   *
+   * @return \Drupal\ai\OperationType\SpeechToSpeech\SpeechToSpeechOutput
+   *   The audio output object.
+   */
+  public function SpeechToSpeech(string|array|SpeechToSpeechInput $input, string $model_id, array $tags = []): SpeechToSpeechOutput;
+
+}
