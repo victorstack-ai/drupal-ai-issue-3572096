02862403273ec19003e11a7e24f6decc98228606
Marcus Johansson <me@marcusmailbox.com>
2024-09-03 20:50:16 +0200

    Issue #3471888 by marcus_johansson: Image Classification Field Validator

---
+++ modules/ai_validations/src/Plugin/FieldValidationRule/AiImageClassificationConstraintFieldValidationRule.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace Drupal\ai_validations\Plugin\FieldValidationRule;
+
+use Drupal\ai\AiProviderPluginManager;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Utility\Token;
+use Drupal\field_validation\ConstraintFieldValidationRuleBase;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides funcationality for AI Image Classification.
+ *
+ * @FieldValidationRule(
+ *   id = "ai_image_classification constraint_rule",
+ *   label = @Translation("AI image classification constraint"),
+ *   description = @Translation("Uses Image classification AI to validate the field.")
+ * )
+ */
+class AiImageClassificationConstraintFieldValidationRule extends ConstraintFieldValidationRuleBase {
+
+
+  /**
+   * The AI provider.
+   *
+   * @var \Drupal\ai\AiProviderPluginManager
+   */
+  protected $aiProvider;
+
+  /**
+   * {@inheritdoc}
+   */
+  final public function __construct($configuration, $plugin_id, $plugin_definition, LoggerInterface $logger, Token $token_service, AiProviderPluginManager $aiProvider) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $logger, $token_service);
+    $this->aiProvider = $aiProvider;
+    $this->setConfiguration($configuration);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('logger.factory')->get('field_validation'),
+      $container->get('token'),
+      $container->get('ai.provider'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConstraintName(): string {
+    return "AiImageClassification";
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isPropertyConstraint(): bool {
+    return TRUE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+      'classification_tag' => NULL,
+      'finder' => 'exact',
+      'model' => NULL,
+      'min_confidence' => 0.8,
+      'message' => NULL,
+    ] + parent::defaultConfiguration();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildConfigurationForm($form, $form_state);
+
+    $options = $this->aiProvider->getSimpleProviderModelOptions('image_classification');
+    $form['model'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Classification Model'),
+      '#options' => $options,
+      '#required' => TRUE,
+      '#default_value' => $this->configuration['model'] ?? '',
+    ];
+
+    $form['classification_tag'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Classification Tag'),
+      '#description' => $this->t('The tag that the image should be classified as.'),
+      '#default_value' => $this->configuration['classification_tag'] ?? '',
+      '#required' => TRUE,
+    ];
+
+    $form['finder'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Finder'),
+      '#options' => [
+        'exact' => $this->t('Exact'),
+        'contains' => $this->t('Contains'),
+      ],
+      '#required' => TRUE,
+      '#default_value' => $this->configuration['finder'] ?? 'exact',
+    ];
+
+    $form['min_confidence'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Minimum Confidence'),
+      '#description' => $this->t('The minimum confidence level required for the classification to trigger.'),
+      '#default_value' => $this->configuration['min_confidence'] ?? 0.8,
+      '#required' => TRUE,
+      '#min' => 0,
+      '#max' => 1,
+      '#step' => 0.001,
+    ];
+
+    $form['model_not_available'] = [
+      'type' => 'select',
+      '#title' => $this->t('If model is not available'),
+      '#options' => [
+        'skip' => $this->t('Skip validation'),
+        'fail' => $this->t('Fail validation'),
+      ],
+      '#default_value' => $this->configuration['model_not_available'] ?? 'skip',
+      '#description' => $this->t('What to do if the model is not available.'),
+    ];
+
+    $message = 'This value is not valid.';
+    $form['message'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Message'),
+      '#default_value' => $this->configuration['message'] ?? $message,
+      '#maxlength' => 255,
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    parent::submitConfigurationForm($form, $form_state);
+
+    $this->configuration['message'] = $form_state->getValue('message');
+    $this->configuration['classification_tag'] = $form_state->getValue('classification_tag');
+    $this->configuration['finder'] = $form_state->getValue('finder');
+    $this->configuration['model'] = $form_state->getValue('model');
+    $this->configuration['min_confidence'] = $form_state->getValue('min_confidence');
+    $this->configuration['model_not_available'] = $form_state->getValue('model_not_available');
+  }
+
+}
---
+++ modules/ai_validations/src/Plugin/Validation/Constraint/AiImageClassificationConstraint.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Drupal\ai_validations\Plugin\Validation\Constraint;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * Ai classification check constraint.
+ *
+ * @Constraint(
+ *   id = "AiImageClassification",
+ *   label = @Translation("AI Image Classification Check", context = "Validation"),
+ * )
+ */
+class AiImageClassificationConstraint extends Constraint {
+
+  /**
+   * The message that will be shown if the constraint is violated.
+   *
+   * @var string
+   */
+  public $message = '';
+
+  /**
+   * The AI model.
+   *
+   * @var string
+   */
+  public $model = '';
+
+  /**
+   * The classification tag.
+   *
+   * @var string
+   */
+  public $classification_tag = '';
+
+  /**
+   * The type of finder.
+   *
+   * @var string
+   */
+  public $finder = '';
+
+ /**
+  * The minimum confidence to pass.
+  *
+  * @var float
+  */
+  public $min_confidence = 0.0;
+
+  /**
+   * The model is not available.
+   *
+   * @var string
+   */
+  public $model_not_available;
+
+}
