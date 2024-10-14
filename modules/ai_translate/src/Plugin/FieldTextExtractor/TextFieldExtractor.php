e86d143e347ddba5b60c80e468e24126521c6d8d
Nick Opris <43140-unqunq@users.noreply.drupalcode.org>
2024-10-14 07:28:18 +0000

    Trim translated text to fit the destination field's length.

--- modules/ai_translate/src/Plugin/FieldTextExtractor/TextFieldExtractor.php
+++ modules/ai_translate/src/Plugin/FieldTextExtractor/TextFieldExtractor.php
@@ -4,6 +4,7 @@ namespace Drupal\ai_translate\Plugin\FieldTextExtractor;
 
 use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Field\FieldConfigInterface;
+use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\ai_translate\Attribute\FieldTextExtractor;
 use Drupal\ai_translate\FieldTextExtractorInterface;
@@ -51,6 +52,18 @@ class TextFieldExtractor implements FieldTextExtractorInterface {
     foreach ($value as $delta => $singleValue) {
       unset($singleValue['field_name'], $singleValue['field_type']);
       $newValue[$delta] = $singleValue;
+      // Trim result if the field definition has a length limit.
+      $field_definition = $entity->getFieldDefinition($fieldName);
+      // Check if the field definition has a max length value.
+      if ($field_definition instanceof FieldDefinitionInterface) {
+        $settings = $field_definition->getSettings();
+        // Retrieve the max length if it exists.
+        $max_length = $settings['max_length'] ?? NULL;
+        if ($max_length !== NULL) {
+          $newValue[$delta]['value'] = mb_strimwidth(
+            $newValue[$delta]['value'], 0, $max_length, '...');
+        }
+      }
     }
     $entity->set($fieldName, $newValue);
   }
