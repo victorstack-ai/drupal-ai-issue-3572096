fdf5b8cce4ddc368f2fe646e7d3270881eb1e25d
Scott Euser <37198-scotteuser@users.noreply.drupalcode.org>
2024-10-16 15:43:24 +0000

    Issue #3481202 by scott_euser: When indexing entity reference fields into the vectors (main or contextual), can we index the label instead of ID?

--- modules/ai_search/src/Plugin/EmbeddingStrategy/EmbeddingBase.php
+++ modules/ai_search/src/Plugin/EmbeddingStrategy/EmbeddingBase.php
@@ -3,6 +3,7 @@
 namespace Drupal\ai_search\Plugin\EmbeddingStrategy;
 
 use Drupal\Component\Utility\Unicode;
+use Drupal\Core\Entity\EntityInterface;
 use Drupal\ai\AiVdbProviderInterface;
 use Drupal\ai\Enum\EmbeddingStrategyCapability;
 use Drupal\ai\Enum\EmbeddingStrategyIndexingOptions;
@@ -171,7 +172,7 @@ class EmbeddingBase extends EmbeddingStrategyPluginBase implements EmbeddingStra
       }
 
       // Get and flatten the value to prepare for conversion to vector.
-      $value = $this->getValue($field);
+      $value = $this->getValue($field, TRUE);
       if (is_array($value)) {
         $value = implode(', ', $value);
       }
@@ -326,7 +327,7 @@ class EmbeddingBase extends EmbeddingStrategyPluginBase implements EmbeddingStra
       ) {
         continue;
       }
-      $metadata[$field->getFieldIdentifier()] = $this->getValue($field);
+      $metadata[$field->getFieldIdentifier()] = $this->getValue($field, FALSE);
     }
     return $metadata;
   }
@@ -377,12 +378,44 @@ class EmbeddingBase extends EmbeddingStrategyPluginBase implements EmbeddingStra
    *
    * @param \Drupal\search_api\Item\FieldInterface $field
    *   The Search API field.
+   * @param bool $convert_to_label
+   *   Convert entity reference target IDs to labels.
    *
    * @return int|string|bool|float
    *   The field value.
    */
-  protected function getValue(FieldInterface $field): int|array|string|bool|float {
+  protected function getValue(FieldInterface $field, bool $convert_to_label): int|array|string|bool|float {
     $values = $field->getValues();
+    try {
+
+      // If the field type is a reference field and its intended to be rendered
+      // as fulltext or a string.
+      $definition = $field->getDataDefinition();
+      $settings = $definition->getSettings();
+      if (
+        in_array($field->getType(), ['fulltext', 'string'])
+        && $definition->getDataType() === 'field_item:entity_reference'
+        && !empty($settings['target_type'])
+      ) {
+
+        // If we can get the entity storage and verify the first entity is
+        // an entity, clear the values and start replacing them with the labels.
+        $storage = $this->entityTypeManager->getStorage($settings['target_type']);
+        $entities = $storage->loadMultiple($values);
+        if ($entities && reset($entities) instanceof EntityInterface) {
+          $values = [];
+          foreach ($entities as $entity) {
+            if (!$entity instanceof EntityInterface) {
+              continue;
+            }
+            $values[$entity->id()] = $entity->label();
+          }
+        }
+      }
+    }
+    catch (\Exception $exception) {
+      // Do nothing, we can just index the values for this type of field.
+    }
 
     // Always composite if field supports multiple. Otherwise, if the field is
     // a single value, we can choose base on the field type At some point we
@@ -406,7 +439,7 @@ class EmbeddingBase extends EmbeddingStrategyPluginBase implements EmbeddingStra
       // Some Vector Databases support arrays, return that in the metadata
       // and leave it to the Provider to flatten if needed.
       $parts = [];
-      foreach ($field->getValues() as $value) {
+      foreach ($values as $value) {
         if (in_array($field->getType(), ['date', 'boolean', 'integer'])) {
           $parts[] = (int) $this->converter->convert((string) $value);
         }
