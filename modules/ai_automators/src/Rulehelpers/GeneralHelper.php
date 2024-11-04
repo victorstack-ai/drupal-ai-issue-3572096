35ce8a8313c2a3091fceb18bda2b9c4e90d66cf7
Marcus Johansson <me@marcusmailbox.com>
2024-11-04 11:41:28 +0100

    Issue #3483197 by alfthecat, marcus_johansson: Text format is escaped until (manual) re-save

--- modules/ai_automators/src/Rulehelpers/GeneralHelper.php
+++ modules/ai_automators/src/Rulehelpers/GeneralHelper.php
@@ -482,7 +482,22 @@ class GeneralHelper {
   }
 
   /**
-   * Get text format.
+   * Get possible text formats for a drop down.
+   *
+   * @return array
+   *   The text formats.
+   */
+  public function getTextFormatsOptions() {
+    $formats = $this->entityTypeManager->getStorage('filter_format')->loadMultiple();
+    $options = [];
+    foreach ($formats as $format) {
+      $options[$format->id()] = $format->label();
+    }
+    return $options;
+  }
+
+  /**
+   * Calculate text format.
    *
    * @param \Drupal\Core\Field\FieldDefinitionInterface $fieldDefinition
    *   The field definition.
@@ -490,7 +505,7 @@ class GeneralHelper {
    * @return string|null
    *   The format.
    */
-  public function getTextFormat(FieldDefinitionInterface $fieldDefinition) {
+  public function calculateTextFormat(FieldDefinitionInterface $fieldDefinition) {
     $allFormats = $this->entityTypeManager->getStorage('filter_format')->loadMultiple();
     // Maybe no formats are available.
     if (empty($allFormats)) {
