83b44372b768641a28b6531c2dbe57e790a42aa5
Fabian Bircher <5370-bircher@users.noreply.drupalcode.org>
2025-05-12 15:00:50 +0000

    ChatGenerator AiApiExplorer plugin calls setChatStructuredJsonSchema on provider instead of input

--- modules/ai_api_explorer/src/Plugin/AiApiExplorer/ChatGenerator.php
+++ modules/ai_api_explorer/src/Plugin/AiApiExplorer/ChatGenerator.php
@@ -283,7 +283,7 @@ final class ChatGenerator extends AiApiExplorerPluginBase {
     }
 
     if ($form_state->getValue('json_schema')) {
-      $provider->setChatStructuredJsonSchema(Json::decode($form_state->getValue('json_schema')));
+      $input->setChatStructuredJsonSchema(Json::decode($form_state->getValue('json_schema')));
     }
 
     $message = NULL;
