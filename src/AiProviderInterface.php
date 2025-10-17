b94a21a950e522840c9cdba63fee84db18830cde
Stefan Korn <drupal@stefan-korn.de>
2025-10-17 09:50:32 +0200

    [#3552782] feat: AiProviderInterface::getConfiguredModels code documentation
    (cherry picked from commit 7d2373c72f1230cf08478d71c5650048b35483b0)

--- src/AiProviderInterface.php
+++ src/AiProviderInterface.php
@@ -12,7 +12,8 @@ interface AiProviderInterface extends PluginInspectionInterface {
   /**
    * Provides associative array with a list of models' IDs.
    *
-   * Keyed with human-readable names and optionally filtered by typ.
+   * Keyed with model IDs, valued with human-readable names
+   * and optionally filtered by type.
    *
    * @param string|null $operation_type
    *   The operation type.
